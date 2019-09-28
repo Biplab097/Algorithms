e8ebae2b2231a98c2888f5b3e58b4b2c7eaa8fa7
Biplab097 <biplabbarman097@gmail.com>
2019-09-28 11:32:14 +0530

    commiting heap_sort

--- heap_sort.c
+++ heap_sort.c
@@ -1,64 +1,71 @@
 #include<stdio.h>
 
-void create(int[]);
-void down_adjust(int[],int);
+void create(int []);
+void down_adjust(int [],int);
 
 int main(int argc[],char const *argv[])
 {
-    FILE *r,*w;
-    r = fopen(argv[1],"r");
-    w = fopen(argv[2],"w");
-    int size,last,i,temp;
-    fscanf(r,"%d",&size);
-    int heap[size+1];
-    for(int i=1;i<=size;i++)
-    {
-        fscanf(r,"%d",&heap[i]);
-    } 
-    heap[0]=size;
-    create(heap);
+	int n,i,last,temp;
+        FILE *r,*w;
+        r = fopen(argv[1],"r");
+        w = fopen(argv[2],"w");
+	//printf("Enter no. of elements:");
+	fscanf(r,"%d",&n);
+        int heap[n];
+	for(i=1;i<=n;i++)
+		fscanf(r,"%d",&heap[i]);
 
-    //sorting
-    while(heap[0]>1)
-    {
-        last = heap[0];  
-        temp=heap[1];
-        heap[1]=heap[last];
-        heap[last]=temp;
-        heap[0]--;
-        down_adjust(heap,1);    
-    }
-    fprintf(w,"Array after sorting\n");
-    for(int i=1;i<=size;i++){
-        fprintf(w,"%d ",&heap[i]);
-    }
-    return 0;
+	//create a heap
+	heap[0]=n;
+	create(heap);
+
+	//sorting
+	while(heap[0] > 1)
+	{
+		//swap heap[1] and heap[last]
+		last=heap[0];
+		temp=heap[1];
+		heap[1]=heap[last];
+		heap[last]=temp;
+		heap[0]--;
+		down_adjust(heap,1);
+	}
+
+	//print sorted data
+	fprintf(w,"\nArray after sorting:\n");
+	for(i=1;i<=n;i++)
+		fprintf(w,"%d ",heap[i]);
+
+	return 0;
 }
+
 void create(int heap[])
 {
-    int i,n;
-    n=heap[0];
-    for(int i=n/2;i>0;i--)
-    {
-        down_adjust(heap,i);
-    }
+	int i,n;
+	n=heap[0]; //no. of elements
+
+	for(i=n/2;i>=1;i--)
+		down_adjust(heap,i);
 }
+
 void down_adjust(int heap[],int i)
 {
-    int j,temp,n,flag=1;
-    n=heap[0];
-    while(2*i<=n && flag==1)
-    {
-        j=2*i;
-        if(j+1<=n && heap[j+1]>heap[j])
-            j=j+1;
-        else if(heap[i]>heap[j])
-            flag=0;
-        else{
-            temp=heap[j];
-            heap[i]=temp;
-            heap[j]=heap[i];
-            i=j;
-        }        
-    }
+	int j,temp,n,flag=1;
+	n=heap[0];
+
+	while(2*i<=n && flag==1)
+	{
+		j=2*i;    //j points to left child
+		if(j+1<=n && heap[j+1] > heap[j])
+			j=j+1;
+		if(heap[i] > heap[j])
+			flag=0;
+		else
+		{
+			temp=heap[i];
+			heap[i]=heap[j];
+			heap[j]=temp;
+			i=j;
+		}
+	}
 }
